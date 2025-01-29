c6604d925abcd62e5c8823ce241984ac23175267
saurabh <tikku.kushwaha@gmail.com>
2025-01-29 12:15:05 +0530

    addedDemo.java

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package gitworkflow;
+
+public class Demo {
+	
+	public void greet()
+	{
+		System.out.println("Hello Evryone");
+	}
+	public void show()
+	{
+		System.out.println("Hello Team , This is Master Build");
+	}
+
+}
